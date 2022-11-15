3f2ef8e65d77f858d08d6d738bb11d582c97ec1a
FloppaSlave <korstim18@gmail.com>
2022-11-15 20:36:48 +0700

    add url for create post

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .import views
+from .views import CreatePostView
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts/', views.PostsListView.as_view(), name='posts'),
     path('register/', views.Register, name='register'),
+    path('create-post/', CreatePostView.as_view(), name='add_post')
 ]
