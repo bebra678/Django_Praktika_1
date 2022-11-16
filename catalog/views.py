e14eb352bf783d6df021d9e723fc37d5dcc3a95f
FloppaSlave <korstim18@gmail.com>
2022-11-16 14:26:53 +0700

    add views for personal_area

--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,11 @@
 from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.views.generic import CreateView
 from .models import Design
 from .forms import UserRegistrationForm, PostForm
-
-
-# Create your views here.
 
 
 def index(request):
@@ -69,4 +67,14 @@ def Logout_view(request):
     logout(request)
     redirect('index.html')
     # Redirect to a success page.
+
+
+class MyDesign(LoginRequiredMixin, generic.ListView):
+    model = Design
+    template_name = "catalog/personal_area.html"
+    context_object_name = 'design_list'
+
+    def get_queryset(self):
+        queryset = Design.objects.filter(user=self.request.user)
+        return queryset
 
