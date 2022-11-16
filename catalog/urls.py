06814c2784962a07c7f64896b06c798e84a3dc2d
FloppaSlave <korstim18@gmail.com>
2022-11-16 14:26:02 +0700

    add url for personal area

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .import views
 from .views import CreatePostView
 
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts/', views.PostsListView.as_view(), name='posts'),
     path('register/', views.Register, name='register'),
-    path('create-post/', CreatePostView.as_view(), name='add_post')
+    path('create-post/', CreatePostView.as_view(), name='add_post'),
+    path('personal-area/', views.MyDesign.as_view(), name='personal_area'),
 ]
