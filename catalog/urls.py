92df470f65b41a18afd3dd5968af8e400fab4135
FloppaSlave <korstim18@gmail.com>
2022-11-17 17:47:50 +0700

    add login_required and add delete/post-control/delete-error urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from .import views
 from .views import CreatePostView
@@ -6,7 +7,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('posts/', views.PostsListView.as_view(), name='posts'),
     path('register/', views.Register, name='register'),
-    path('create-post/', CreatePostView.as_view(), name='add_post'),
-    path('personal-area/', views.MyDesign.as_view(), name='personal_area'),
+    path('create-post/', login_required(CreatePostView.as_view()), name='add_post'),
+    path('personal-area/', login_required(views.MyDesign.as_view()), name='personal_area'),
+    path('delete/<int:pk>', views.DeletePost.as_view(), name='delete_post'),
+    path('post-control/', login_required(views.PostControl.as_view()), name='post_control'),
+    path('delete-error/', views.get_error, name='error'),
     path('change-posts/', views.AdminListView.as_view(), name='change_post')
 ]
