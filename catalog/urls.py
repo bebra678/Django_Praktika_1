0f2dbac4148a9c5483de7b32ab59c6766e5fa618
FloppaSlave <korstim18@gmail.com>
2022-11-18 23:15:31 +0700

    refactor html(delete login_required)

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,17 +1,15 @@
-from django.contrib.admin.views.decorators import staff_member_required
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import permission_required
 from django.urls import path
 from .import views
-from .views import CreatePostView
+from .views import CreatePostView, IndexView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', IndexView.as_view(template_name="index.html"), name='index'),
     path('posts/', views.PostsListView.as_view(), name='posts'),
-    path('register/', views.Register, name='register'),
-    path('create-post/', login_required(CreatePostView.as_view()), name='add_post'),
-    path('personal-area/', login_required(views.MyDesign.as_view()), name='personal_area'),
-    path('delete/<int:pk>', views.DeletePost.as_view(), name='delete_post'),
+    path('register/', views.register, name='register'),
+    path('create-post/', CreatePostView.as_view(), name='add_post'),
+    path('personal-area/', views.MyDesign.as_view(), name='personal_area'),
+    path('delete/<int:pk>/', views.DeletePost.as_view(), name='delete_post'),
     path('post-control/', permission_required('change_post')(views.PostControl.as_view()), name='post_control'),
-    path('delete-error/', views.get_error, name='error'),
-    path('update-category/<int:pk>', permission_required('change_post')(views.PostUpdate.as_view()), name='update_form'),
+    path('update-category/<int:pk>/', permission_required('change_post')(views.PostUpdate.as_view()), name='update_form'),
 ]
