9f7a5a18431d25ad3d3118093651f3e895e61e05
FloppaSlave <korstim18@gmail.com>
2022-11-19 17:23:27 +0700

    def my_view = def login_view / class DeletePost - refactor / class PostUpdate - refactor

--- catalog/views.py
+++ catalog/views.py
@@ -27,6 +27,7 @@ class CreatePostView(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy('posts')
 
     def form_valid(self, form):
+        # super - функция, которая обращается к классу, от которого наследуется текущий.
         form.instance.user = self.request.user
         return super(CreatePostView, self).form_valid(form)
 
@@ -36,11 +37,8 @@ def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            # Create a new user object but avoid saving it yet
             new_user = user_form.save(commit=False)
-            # Set the chosen password
             new_user.set_password(user_form.cleaned_data['password'])
-            # Save the User object
             new_user.save()
             return render(request, 'register_done.html', {'new_user': new_user})
     else:
@@ -49,21 +47,18 @@ def register(request):
 
 
 #FIXME: lower word in def
-def my_view(request):
+def login_view(request):
     username = request.POST['username']
     password = request.POST['password']
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
         redirect('index.html')
-    else:
-        pass
 
 
 def logout_view(request):
     logout(request)
     redirect('index.html')
-    # Redirect to a success page.
 
 
 class MyDesign(LoginRequiredMixin, ListView):
@@ -80,34 +75,18 @@ class DeletePost(DeleteView):
     model = Design
     success_url = reverse_lazy('post_control')
 
-    def form_valid(self, form):
-        if self.object.status != 'new':
-            return redirect('catalog/error_delete.html')
-        else:
-            self.object.delete()
-            success_url = reverse_lazy('profile_applications')
-            success_msg = 'Запись удалена'
-            return HttpResponseRedirect(success_url, success_msg)
+    def form_valid(self):
+        self.object.delete()
 
 
 class PostControl(ListView):
     model = Design
     template_name = 'catalog/post_control.html'
-    success_url = reverse_lazy('post_control')
 
 
 class PostUpdate(UpdateView):
     model = Design
-    fields = ('status', 'ready_images', 'category', 'comment')
+    fields = ('status', 'image', 'category', 'comment')
     template_name = 'catalog/update_form.html'
     success_url = reverse_lazy('post_control')
 
-    def get_context_data(self):
-        context = super().get_context_data()
-        if self.object.status == 'ready':
-            context['is_ready'] = True
-        elif self.object.status == 'load':
-            context['is_load'] = True
-        elif self.object.status == 'new':
-            context['is_new'] = True
-        return context
