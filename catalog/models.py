38d1df70aa3324cac3c6711779a8fdfaf8969148
FloppaSlave <korstim18@gmail.com>
2022-11-19 17:20:44 +0700

    DB - migrate / Models - delete readi_images field

--- catalog/models.py
+++ catalog/models.py
@@ -9,7 +9,6 @@ class Design(models.Model):
                             error_messages={'unique':"Такая заявка уже существует!"})
     info = models.CharField(max_length=50, help_text='Введите описание', verbose_name='Описание', null=True)
     image = models.ImageField(upload_to='images/',  verbose_name='Изображение', null=False)
-    ready_images = models.ImageField(upload_to='ready/', verbose_name='Готовый дизайн', null=False, blank=True)
     date = models.DateField(default=datetime.today(), null=True, verbose_name='Дата')
     user = models.ForeignKey(User, on_delete=models.SET_NULL, verbose_name='Пользователь', null=True, blank=True,
                              to_field='id')
@@ -45,5 +44,4 @@ class Design(models.Model):
 
     def __str__(self):
         return self.name
-
 
