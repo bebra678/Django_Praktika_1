fc7427e573526868fb965d6aeaca9b376a8bd227
FloppaSlave <korstim18@gmail.com>
2022-11-20 15:56:30 +0700

    add ulr for add category

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.decorators import permission_required
 from django.urls import path
 from .import views
-from .views import CreatePostView, IndexView
+from .views import CreatePostView, IndexView, CreateCategoryView
 
 urlpatterns = [
     path('', IndexView.as_view(template_name="index.html"), name='index'),
     path('posts/', views.PostsListView.as_view(), name='posts'),
     path('register/', views.register, name='register'),
     path('create-post/', CreatePostView.as_view(), name='add_post'),
+    path('create-category/', CreateCategoryView.as_view(), name='add_category'),
     path('personal-area/', views.MyDesign.as_view(), name='personal_area'),
     path('delete/<int:pk>/', views.DeletePost.as_view(), name='delete_post'),
     path('delete-by-user/<int:pk>/', views.DeletePostByUser.as_view(), name='delete_post_by_user'),
