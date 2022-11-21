df185671e0b6d12a74a72d0035085753d004ba42
FloppaSlave <korstim18@gmail.com>
2022-11-21 22:17:42 +0700

    delete posts ulr / delete permission_required in post_control / change url name

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,7 +5,6 @@ from .views import CreatePostView, IndexView, CreateCategoryView
 
 urlpatterns = [
     path('', IndexView.as_view(template_name="index.html"), name='index'),
-    path('posts/', views.PostsListView.as_view(), name='posts'),
     path('register/', views.register, name='register'),
     path('create-post/', CreatePostView.as_view(), name='add_post'),
     path('create-category/', CreateCategoryView.as_view(), name='add_category'),
@@ -15,8 +14,7 @@ urlpatterns = [
     path('delete-category/<int:pk>/', permission_required('change_post')
     (views.DeleteCategory.as_view()), name='delete_category'),
     path('delete-by-user/<int:pk>/', views.DeletePostByUser.as_view(), name='delete_post_by_user'),
-    path('post-control/', permission_required('change_post')(views.post_control),
-         name='post_control'),
-    path('update-category/<int:pk>/', permission_required('change_post')(views.PostUpdate.as_view())
+    path('post-control/', views.post_control, name='post_control'),
+    path('update-post/<int:pk>/', permission_required('change_post')(views.PostUpdate.as_view())
          , name='update_form'),
 ]
