2e72092e2f4b7933c4b20e2518d6667b48586264
ledxdeliveryflopp <korstim18@gmail.con>
2022-11-23 13:15:11 +0700

    add check for user session

--- catalog/views.py
+++ catalog/views.py
@@ -90,16 +90,25 @@ class PostUpdateReady(UpdateView):
 
 # регистрация
 def register(request):
-    if request.method == 'POST':
-        user_form = UserRegistrationForm(request.POST)
-        if user_form.is_valid():
-            new_user = user_form.save(commit=False)
-            new_user.set_password(user_form.cleaned_data['password'])
-            new_user.save()
-            return render(request, 'register_done.html', {'new_user': new_user})
+    if request.user.id:
+        return redirect('index')
     else:
-        user_form = UserRegistrationForm()
-    return render(request, 'register.html', {'user_form': user_form})
+        if request.method == 'POST':
+            user_form = UserRegistrationForm(request.POST)
+            if user_form.is_valid():
+                new_user = user_form.save(commit=False)
+                new_user.set_password(user_form.cleaned_data['password'])
+                new_user.save()
+                return render(request, 'register_done.html', {'new_user': new_user})
+        else:
+            user_form = UserRegistrationForm()
+        return render(request, 'register.html', {'user_form': user_form})
+
+#
+# class SignUpView(CreateView):
+#     form_class = UserRegistrationForm
+#     success_url = reverse_lazy('login')
+#     template_name = 'register.html'
 
 
 # логин
